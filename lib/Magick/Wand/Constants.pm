a38a52959cc7c270e8255c329419c1ede7a46569
Meredith Howard <mhoward@roomag.org>
2019-10-05 14:48:27 -0500

    add constants docs

--- lib/Magick/Wand/Constants.pm
+++ lib/Magick/Wand/Constants.pm
@@ -209,3 +209,76 @@ our @EXPORT_OK = (
 );
 
 1;
+__END__
+=head1 NAME
+
+Magick::Wand::Constants - MagickWand API Constants
+
+=head1 SYNOPSIS
+
+  use Magick::Wand::Constants 'NoiseType';
+  ...;
+  $wand->add_noise_image(NoiseType->{GaussianNoise}, 1.25);
+
+  use Magick::Wand::Constants 'GaussianNoise';
+  ...;
+  $wand->add_noise_image(GaussianNoise, 1.25);
+
+=head1 DESCRIPTION
+
+Magick::Wand::Constants exports constants for use with L</Magick::Wand>, which
+are mostly enumerations.  For any enumeration, you can import the enumeration
+name, which is a constant sub returning a hashref, or you can import specific
+values from within any enumeration as a numeric constant.
+
+Why would you use one or the other?  Using a hashref may be easier if you need
+to vary at runtime, while using a numeric constant can protect against typos
+(moving the failure to startup rather than runtime) if your value is
+unchanging.
+
+=head2 ImageMagick 6 Compatibility
+
+Some enums have changed since ImageMagick 6.  We're primarily supporting 7, so
+if an enum's name has changed, we prefer the newer name.  The options within
+that enum should match the ImageMagick version that we were able to load at
+startup (see L<Magick::Wand::API>).
+
+Be aware that this means if you try to import a numeric constant that isn't
+present in the loaded version of ImageMagick, you should get a compile-time
+error.  Hashref constants, on the other hand, are locked, and will trigger
+a run-time error if you try to use a key that isn't present.
+
+=head1 CONSTANTS
+
+This documention only lists the enum names (hashref constants) available, in
+part because the keys within them may depend on your environment.  For more
+info you can check the module source, ImageMagick and MagickWand documentation,
+and their source.
+
+You can display the numeric constants currently available:
+
+  perl -MMagick::Wand::Constants -E 'say for sort keys %{$Magick::Wand::Constants::const{AlphaChannelOption}}'
+
+Substituting C<AlphaChannelOption> with the enum name you'd like to see.
+
+=over
+
+=item AlphaChannelOption
+
+=item CompositeOperator
+
+=item ImageType
+
+=item InterlaceType
+
+=item MagickBooleanType
+
+=item NoiseType
+
+=item OrientationType
+
+=item ResolutionType
+
+=back
+
+=cut
