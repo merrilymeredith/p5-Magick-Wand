617b36b31d1785a0e911396fb0c02af16ba510c1
Meredith Howard <mhoward@roomag.org>
2019-10-05 04:37:57 -0500

    Add some constants!
    Some may not match up between im6 and im7, I'll need to handle that if
    I want to support 6.

--- lib/Magick/Wand/Constants.pm
+++ lib/Magick/Wand/Constants.pm
@@ -3,14 +3,115 @@ package Magick::Wand::Constants;
 use warnings;
 use strict;
 
+use parent 'Exporter';
+
+sub enum { my $i = 0; map {$_ => $i++} @_ }
+
+our %const;
+
+BEGIN {
+  %const = (
+    MagickBooleanType => {enum qw/
+      MagickFalse
+      MagickTrue
+    /},
+
+    NoiseType => {enum qw/
+      UndefinedNoise
+      UniformNoise
+      GaussianNoise
+      MultiplicativeGaussianNoise
+      ImpulseNoise
+      LaplacianNoise
+      PoissonNoise
+      RandomNoise
+    /},
+
+    CompositeOperator => {enum qw/
+      UndefinedCompositeOp
+      AlphaCompositeOp
+      AtopCompositeOp
+      BlendCompositeOp
+      BlurCompositeOp
+      BumpmapCompositeOp
+      ChangeMaskCompositeOp
+      ClearCompositeOp
+      ColorBurnCompositeOp
+      ColorDodgeCompositeOp
+      ColorizeCompositeOp
+      CopyBlackCompositeOp
+      CopyBlueCompositeOp
+      CopyCompositeOp
+      CopyCyanCompositeOp
+      CopyGreenCompositeOp
+      CopyMagentaCompositeOp
+      CopyAlphaCompositeOp
+      CopyRedCompositeOp
+      CopyYellowCompositeOp
+      DarkenCompositeOp
+      DarkenIntensityCompositeOp
+      DifferenceCompositeOp
+      DisplaceCompositeOp
+      DissolveCompositeOp
+      DistortCompositeOp
+      DivideDstCompositeOp
+      DivideSrcCompositeOp
+      DstAtopCompositeOp
+      DstCompositeOp
+      DstInCompositeOp
+      DstOutCompositeOp
+      DstOverCompositeOp
+      ExclusionCompositeOp
+      HardLightCompositeOp
+      HardMixCompositeOp
+      HueCompositeOp
+      InCompositeOp
+      IntensityCompositeOp
+      LightenCompositeOp
+      LightenIntensityCompositeOp
+      LinearBurnCompositeOp
+      LinearDodgeCompositeOp
+      LinearLightCompositeOp
+      LuminizeCompositeOp
+      MathematicsCompositeOp
+      MinusDstCompositeOp
+      MinusSrcCompositeOp
+      ModulateCompositeOp
+      ModulusAddCompositeOp
+      ModulusSubtractCompositeOp
+      MultiplyCompositeOp
+      NoCompositeOp
+      OutCompositeOp
+      OverCompositeOp
+      OverlayCompositeOp
+      PegtopLightCompositeOp
+      PinLightCompositeOp
+      PlusCompositeOp
+      ReplaceCompositeOp
+      SaturateCompositeOp
+      ScreenCompositeOp
+      SoftLightCompositeOp
+      SrcAtopCompositeOp
+      SrcCompositeOp
+      SrcInCompositeOp
+      SrcOutCompositeOp
+      SrcOverCompositeOp
+      ThresholdCompositeOp
+      VividLightCompositeOp
+      XorCompositeOp
+      StereoCompositeOp
+    /},
+  );
+}
+
 use constant {
-  MagickFalse => 0,
-  MagickTrue  => 1,
+  %const,
+  map {%$_} grep {(ref($_) // '') eq 'HASH'} values %const,
 };
 
-our @EXPORT_OK = qw/
-  MagickFalse
-  MagickTrue
-/;
+our @EXPORT_OK = (
+  keys %const,
+  map {keys %$_} grep {(ref($_) // '') eq 'HASH'} values %const,
+);
 
 1;
